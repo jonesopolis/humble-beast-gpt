d6b9ff2b6e459a94a87611a07aae63599739a234
David Rector <drector@bluemodus.com>
2024-03-02 07:28:31 -0700

    test

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,45 @@
 import OpenAI from 'openai'
 import { ref } from 'vue'
 
-const key = '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'
+const key = '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';
 const openai = new OpenAI({ apiKey: key, dangerouslyAllowBrowser: true })
 
 const prompt = ref('')
 const result = ref('')
+const imageResult = ref('')
 const loading = ref(false)
 
 let query = async () => {
   result.value = '';
+  imageResult.value = '';
   loading.value = true
 
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'system', content: prompt.value }],
     model: 'gpt-3.5-turbo'
   })
-
+  
   loading.value = false
   result.value = completion.choices[0].message.content
   console.log(completion.choices[0])
+}
+
+let queryImage = async () => {
+  result.value = '';
+  imageResult.value = '';
+  loading.value = true
+
+  const image = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: prompt.value,
+    n: 1,
+    size: "1024x1024",
+  });
+
+  console.log(image.data[0].url);
+
+  imageResult.value = image.data[0].url;
+  loading.value = false
 }
 </script>
 
@@ -43,16 +63,17 @@ let query = async () => {
       <v-container>
         <v-row>
           <v-col cols="12" md="2">
-            <v-sheet rounded="lg" min-height="268">
-              <!--  -->
-            </v-sheet>
+<!--            <v-sheet rounded="lg" min-height="268">-->
+<!--              &lt;!&ndash;  &ndash;&gt;-->
+<!--            </v-sheet>-->
           </v-col>
 
           <v-col cols="12" md="8">
             <v-col cols="12" md="12">
               <v-sheet min-height="40vh" rounded="lg">
                 <v-textarea label="Prompt" variant="solo" v-model="prompt" v-on:keyup.enter="query()"></v-textarea>
-                <v-btn prepend-icon="$vuetify" @click="query()" min-width="100%"> ASK </v-btn>
+                <v-btn prepend-icon="$vuetify" @click="query()" min-width="100%"> TEXT </v-btn>
+                <v-btn prepend-icon="$vuetify" @click="queryImage()" min-width="100%"> IMAGE </v-btn>
               </v-sheet>
             </v-col>
 
@@ -62,19 +83,20 @@ let query = async () => {
                   <v-progress-linear
                     v-if="loading"
                     class="my-5"
-                    indeterminate
+                    indeterminate="true"
                     color="cyan"
                   ></v-progress-linear>
                 </div>
-                <p class="text-left" v-html="result"></p>
+                <img v-if="imageResult" class="text-left" :src="imageResult" width="100%"/>
+                <p v-if="result" class="text-left" v-html="result"></p>
               </v-sheet>
             </v-col>
           </v-col>
 
           <v-col cols="12" md="2">
-            <v-sheet rounded="lg" min-height="268">
-              <!--  -->
-            </v-sheet>
+<!--            <v-sheet rounded="lg" min-height="268">-->
+<!--              &lt;!&ndash;  &ndash;&gt;-->
+<!--            </v-sheet>-->
           </v-col>
         </v-row>
       </v-container>
